84fabad47fe31218a6a68ab7ee8188f676558a4e
udaybhushan t <udaybhushant@nbos.vinisoft.com>
2020-05-13 16:27:40 +0530

    added jwttoken project

---
+++ src/main/java/com/example/demo/controller/JwtController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JwtController {
+	
+	@RequestMapping({"/hello"})
+	public String hello() {
+		return "Hello World";
+	}
+}
